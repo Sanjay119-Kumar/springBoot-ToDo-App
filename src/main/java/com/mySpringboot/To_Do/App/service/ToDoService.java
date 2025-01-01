c4dc75503d6234e223b80461008f6080436bac85
Sanjay Kumar <college.0119@gmail.com>
2025-01-01 14:48:58 +0530

    src/main/java/com/mySpringboot/To_Do/App/service/ToDoService.java

--- src/main/java/com/mySpringboot/To_Do/App/service/ToDoService.java
+++ src/main/java/com/mySpringboot/To_Do/App/service/ToDoService.java
@@ -15,6 +15,11 @@ public class ToDoService {
     @Autowired
     private ToDoRepository toDoRepository;
 
+    // Method to save a ToDo entity
+    public ToDoEntity save(ToDoEntity toDoEntity) {
+        return toDoRepository.save(toDoEntity);
+    }
+
     // Fetch all todo
     public List<ToDoEntity> getAllTodos(){
        return toDoRepository.findAll();
